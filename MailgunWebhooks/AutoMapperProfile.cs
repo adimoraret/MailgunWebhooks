b0123de19f068160e52dbdc28d268489d23d3272
Adrian Moraret <adrian.moraret@yahoo.com>
2017-08-22 20:06:49 -0700

    Added AutoMapper package

---
+++ MailgunWebhooks/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using MailgunWebhooks.Models;
+
+namespace MailgunWebhooks
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            MapMailgunWebhooks();
+        }
+
+        private void MapMailgunWebhooks()
+        {
+            MapMailgunWebhookRequest();
+        }
+
+        private void MapMailgunWebhookRequest()
+        {
+        }
+    }
+}
